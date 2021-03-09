3eb2b0a8b4689fbb7fab32a082acb456763077ba
bryan <bryanibit@gmail.com>
2021-03-10 00:16:56 +0800

    simplify belief update

--- include/occ_grid_mapping/grid_map.h
+++ include/occ_grid_mapping/grid_map.h
@@ -13,8 +13,8 @@ public:
     GridMap(const int& size_x, const int& size_y, const int& init_x, const int& init_y, const double& cell_size );
     bool setGridBel(const double& x, const double& y, const double& bel);
     bool getGridBel ( const double& x, const double& y, double& bel);
-    bool setGridLogBel(const double& x, const double& y, const double& log_bel);
-    bool getGridLogBel(const double& x, const double& y, double& log_bel);
+    // bool setGridLogBel(const double& x, const double& y, const double& log_bel);
+    // bool getGridLogBel(const double& x, const double& y, double& log_bel);
     double getCellSize();
    
     void toRosOccGridMap(const std::string& frame_id, nav_msgs::OccupancyGrid& occ_grid); //转换到Ros栅格地图的消息
@@ -22,7 +22,7 @@ public:
     void saveMap(const std::string& img_dir, const std::string& cfg_dir); // 保存地图，图片加配置文件的形式
     void loadMap(const std::string& img_dir, const std::string& cfg_dir); // TODO 加载地图
     
-private:
+//private:
     bool getIdx(const double& x, const double& y, Eigen::Vector2i& idx);
     
 private:
